f797f427f9cde567b657e0fcd5f688586acf2c54
weibaohui <weibaohui@yeah.net>
2020-10-08 20:32:25 +0800

    update

--- gitlab/start.sh
+++ gitlab/start.sh
@@ -1,5 +1,7 @@
 docker run -it --rm  \
 -p 9443:443 -p 9080:80 -p 9022:22 \
+--hostname git.sdibt.com \
+--env 'GITLAB_TIMEZONE=Beijing' \
 -v $(pwd)/gitlab/config:/etc/gitlab \
 -v $(pwd)/gitlab/logs:/var/log/gitlab \
 -v $(pwd)/gitlab/data:/var/opt/gitlab \
