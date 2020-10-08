1498cd8e8647a3143780a495c21778e94d97c1be
weibaohui <weibaohui@yeah.net>
2020-10-08 18:10:31 +0800

    ok

--- gitlab/start.sh
+++ gitlab/start.sh
@@ -1,6 +1,6 @@
 docker run -it --rm  \
 -p 9443:443 -p 9080:80 -p 9022:22 \
--v ~/gitlab/config:/etc/gitlab \
--v ~/gitlab/logs:/var/log/gitlab \
--v ~/gitlab/data:/var/opt/gitlab \
+-v $(pwd)/gitlab/config:/etc/gitlab \
+-v $(pwd)/gitlab/logs:/var/log/gitlab \
+-v $(pwd)/gitlab/data:/var/opt/gitlab \
 gitlab/gitlab-ce
