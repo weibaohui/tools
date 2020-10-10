c2e4ce43d3ce98bfd607f4f844ea6ddeb6cb27ed
weibaohui <weibaohui@yeah.net>
2020-10-11 00:50:03 +0800

    kafka

--- kafka/start.sh
+++ kafka/start.sh
@@ -1,2 +1,3 @@
+docker rm zookeeper
 docker run -d --name zookeeper -p 2181:2181  wurstmeister/zookeeper
-docker run -it --name kafka -p 9092:9092 -e KAFKA_BROKER_ID=0 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 --link zookeeper -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.1.45:9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -t wurstmeister/kafka
+docker run -it  -p 9092:9092 -e KAFKA_BROKER_ID=0 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 --link zookeeper -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.1.45:9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -t wurstmeister/kafka
